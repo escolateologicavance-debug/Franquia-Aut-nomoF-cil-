bb95534036886408b24f03f63497f7347da6660d
Gilberto Moreira <escolateologicavance@gmail.com>
2026-06-20 14:06:56 -0400

    Update service worker for cache versioning and fetch handling

--- sw.js
+++ sw.js
@@ -1,17 +1,40 @@
-const CACHE_NAME = 'autonomo-facil-v1';
+const CACHE_NAME = 'autonomo-facil-v2';
+
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/logo-192.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './logo-192.png',
+  './logo-512.png'
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    fetch(event.request)
+      .then(response => response)
+      .catch(() => caches.match(event.request))
   );
 });
