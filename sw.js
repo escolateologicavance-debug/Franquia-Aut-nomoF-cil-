4a4a97e62f26d54973c695bf6ab104017678ca2b
Gilberto Moreira <escolateologicavance@gmail.com>
2026-06-20 13:28:14 -0400

    Update service worker cache version to v2

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (e) => {
 e.waitUntil(
-caches.open('autonomo-facil-v1').then((cache) => {
+caches.open('autonomo-facil-v2').then((cache) => {
 return cache.addAll(['/', 'index.html', 'manifest.json', 'logo-192.png']);
 })
 );
