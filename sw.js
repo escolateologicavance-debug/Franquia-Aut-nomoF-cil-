5de5e5badcba0c819e7e85521ebbed951a25a0c5
Gilberto Moreira <escolateologicavance@gmail.com>
2026-06-20 13:48:49 -0400

    Refactor service worker to use constants for cache

--- sw.js
+++ sw.js
@@ -1,15 +1,17 @@
-self.addEventListener('install', (e) => {
-e.waitUntil(
-caches.open('autonomo-facil-v2').then((cache) => {
-return cache.addAll(['/', 'index.html', 'manifest.json', 'logo-192.png']);
-})
-);
+const CACHE_NAME = 'autonomo-facil-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/logo-192.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-self.addEventListener('fetch', (e) => {
-e.respondWith(
-caches.match(e.request).then((response) => {
-return response || fetch(e.request);
-})
-);
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
 });
