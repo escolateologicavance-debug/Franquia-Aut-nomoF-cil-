9fadaca05a698c3802ce342ff5754ee20dc51145
Gilberto Moreira <escolateologicavance@gmail.com>
2026-06-20 14:24:55 -0400

    Update service worker cache version and logic
    Updated CACHE_NAME to version 3 and simplified the activate event listener.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'autonomo-facil-v2';
+const CACHE_NAME = 'autonomo-facil-v3';
 
 const ASSETS = [
   './',
@@ -17,24 +17,12 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
-  self.clients.claim();
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     fetch(event.request)
-      .then(response => response)
       .catch(() => caches.match(event.request))
   );
 });
